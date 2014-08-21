28df78e46c6168b77b054cbb77ab0a82592dd3b2
Sebastian M. Perez <perezsebastianm@gmail.com>
2014-08-21 01:34:41 -0300

    Create the layout with the required components

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,12 @@
-console.log('\'Allo \'Allo!');
+'use strict';
+
+$(function(){
+	$('.otherProductsCarousel').carousel({
+		'limitPerPage': 3,
+		'pagination': false
+	});
+
+
+	var tabs = $('.moreInfoTabs').tabs();
+	tabs.show(2);
+});
